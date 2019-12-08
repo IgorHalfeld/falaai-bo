462e867e5918e01b562a6b579001425f66e541df
Igor Halfeld <hello@igorluiz.me>
2019-12-07 22:45:28 -0300

    add proxy

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import GeneralService from './general';
 
 const generalHttpClient = axios.create({
-  baseURL: 'http://aplicacoes.portodesantos.com.br:9104/siap/servicos/atracacao/siteweb/',
+  baseURL: 'https://proxier.now.sh/http://aplicacoes.portodesantos.com.br:9104/siap/servicos/atracacao/siteweb/',
   headers: {
     Host: 'aplicacoes.portodesantos.com.br:9104',
     Origin: 'http://www.portodesantos.com.br',
